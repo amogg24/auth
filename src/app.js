d2192601e125546bb576ac0ec981f74b2e63a604
Andrew Mogg <andrewmogg@Andrews-MacBook-Pro.local>
2017-07-14 15:00:40 -0400

    updated ignore

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBKHR4hMZgA5t0MehDBKjHW3FWI1KFkSkg",
+      authDomain: "auth-5e6e3.firebaseapp.com",
+      databaseURL: "https://auth-5e6e3.firebaseio.com",
+      projectId: "auth-5e6e3",
+      storageBucket: "auth-5e6e3.appspot.com",
+      messagingSenderId: "878615570332"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
